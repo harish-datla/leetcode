bad99239bb7cdaa1f9ba26dab74746066bdb32cd
harish-datla <36021808+harish-datla@users.noreply.github.com>
2020-08-06 03:13:32 +0530

    24 ms

--- solutions/3. Longest Substring Without Repeating Characters.cpp
+++ solutions/3. Longest Substring Without Repeating Characters.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-       map<int,int> mp;
-        map<int,int> k;
-       int i = 0 ; int j = 0;
-        int ans = 0;
-        int count = 0;
-        for(int j = 0 , i = 0 ; j < s.size() ; j++)
-        {
-            if(mp[s[j]])
-            {
-                i = max(mp[s[j]],i);
-            }
-            ans = max(ans,j-i+1);
-            mp[s[j]] = j+1;
-        }
-        
-        return ans;
-        
-    }
+    int lengthOfLongestSubstring(string s) {
+        if(s.size() == 0)return 0;
+        if(s.size() == 1)return 1;
+        unordered_map<char,int> index;
+        int i= 0, j = 1;
+        while(s[i] == s[j] && j < s.size()){
+            i++;j++;
+        }
+        if(j == s.size())return 1;
+        index[s[i]] = i;
+        int maxi = INT_MIN;
+        maxi = max(maxi,j-i+1);
+        while( j < s.size() && i <s.size()){
+            if((index[s[j]] > i && index[s[j]]!=j) || s[j] == s[i] ){
+                i = index[s[j]]+1;
+                index[s[j]] = j;
+                if(i == j){j++;}
+                continue;  
+            }
+            index[s[j]] = j;
+            maxi = max(maxi,j-i+1);
+            j++;
+        }
+        return maxi;
+    }
 };
