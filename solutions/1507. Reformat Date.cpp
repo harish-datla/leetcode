5dc5e4c2b8056765ddd4b6fb425cfe882df3cfdf
harish-datla <36021808+harish-datla@users.noreply.github.com>
2020-08-06 23:02:07 +0530

    4 ms

--- solutions/1507. Reformat Date.cpp
+++ solutions/1507. Reformat Date.cpp
@@ -1,28 +1,24 @@
+            }
+            else
+            {
+                ans.push_back(ch);
+                ch = "";
+            }
         }
-        map<string,string> mp;
-        mp["Jan"]="01";
-        mp["Feb"]="02";
-        mp["Mar"]="03";
-        mp["Apr"]="04";
-        mp["May"]="05";
-        mp["Jun"]="06";
-        mp["Jul"]="07";
-        mp["Aug"]="08";
-        mp["Sep"]="09";
-        mp["Oct"]="10";
-        mp["Nov"]="11";
-        mp["Dec"]="12";
-        int idx=date.find(' ');
-        string temp=date.substr(idx+1);
-        int idx2=temp.find(' ');
-        string month=temp.substr(0,idx2);
-        string left=temp.substr(idx2+1);
+        ans.push_back(ch);
+        string out = "";
+        ans[0].pop_back();
+        ans[0].pop_back();
+        out+=ans[2];
+        out+="-";
+        out+=(mp[ans[1]]);
+        out+="-";
+        if(stoi(ans[0]) < 10){
+            out+="0";
+        }
+        out+=ans[0];
         
-        cout<<month<<endl;
-        while(left.size()<4){
-            left="0"+left;
-        }
-        string result=left+"-"+mp[month]+"-"+day;
-        return result;
+        return out;
+        
     }
 };
