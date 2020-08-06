11c07c1fb91ea968d37f5b05f435c704179d659a
harish-datla <36021808+harish-datla@users.noreply.github.com>
2020-08-07 00:35:06 +0530

    180 ms

--- solutions/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
+++ solutions/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
@@ -1,15 +1,57 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        if(nums.size() <= 4)return 0;
-        if(nums.size() == 5){
-            
+        int result;
+        if (nums.size() <= 4) return 0;
+        int min1 = INT_MAX, min2 = INT_MAX, min3 = INT_MAX, min4 = INT_MAX;
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN, max4 = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < min1) {
+                min4 = min3;
+                min3 = min2;
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if (nums[i] < min2) {
+                min4 = min3;
+                min3 = min2;
+                min2 = nums[i];
+            }
+            else if (nums[i] < min3) {
+                min4 = min3;
+                min3 = nums[i];
+            }
+            else if (nums[i] < min4) {
+                min4 = nums[i];
+            }
+            if (nums[i] > max1) {
+                max4 = max3;
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if (nums[i] > max2) {
+                max4 = max3;
+                max3 = max2;
+                max2 = nums[i];                
+            }
+            else if (nums[i] > max3) {
+                max4 = max3;
+                max3 = nums[i];               
+            }
+            else if (nums[i] > max4) {
+                max4 = nums[i];                
+            }
         }
-        sort(nums.begin(),nums.end());
-        return min(nums[nums.size()-4] - nums[0],min(nums[nums.size() -1] - nums[3],min(nums[nums.size() - 2] - nums[2],nums[nums.size() - 3]- nums[1])));
         
-      
+        result = min(max4-min1, max1-min4);
+        result = min(max2-min3, result);
+        result = min(max3-min2, result);
+        return result;
     }
 };
-​
-​
+static const auto magic = []{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  return nullptr;
+}();
