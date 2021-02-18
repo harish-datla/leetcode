5a3009c9fe6a28baf0d4304fc3d20fc2741adc4d
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-18 14:46:22 +0530

    Update two-sum-ii-input-array-is-sorted.go

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,3 +1,29 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
 func twoSum(numbers []int, target int) []int {
     i := 0
     j := len(numbers) - 1
