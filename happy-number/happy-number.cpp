6adc1a293cb99637c4ebb693f3ca83cc23c93d71
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-19 16:24:01 +0530

    Time: 4 ms (52.62%), Space: 6.1 MB (71.28%) - LeetHub

---
+++ happy-number/happy-number.cpp
@@ -0,0 +1,48 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+class Solution {
+public:
+    bool isHappy(int n) {
+        map<int,bool> mp;
+        while(true){
+            int sum = 0;
+            while(n > 0){
+                sum = sum + (n%10)*(n%10);
+                n = n/10;
+            }
+            if(mp[sum]){
+                return false;
+            }
+            if(sum == 1){
+                return true;
+            }
+            mp[sum] = true;
+            n = sum;
+        }
+        return true;
+    }
+};
