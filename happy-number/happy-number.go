d9bc008725673d291f7245b004df2c87810797a7
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-19 16:14:59 +0530

    Time: 0 ms (100.00%), Space: 2.1 MB (78.42%) - LeetHub

---
+++ happy-number/happy-number.go
@@ -0,0 +1,49 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+func isHappy(n int) bool {
+    mp := map[int]bool{
+        
+    }
+    for ; true ; {
+        sum := 0
+        for ; n > 0  ; {
+            sum+=((n%10)*(n%10))
+            n = n / 10
+        }
+        if _,ok := mp[sum]; ok {
+            return false
+        }
+        if sum == 1{
+            return true
+        }
+        mp[sum] = true
+        n = sum
+    } 
+    
+    return true
+    
+}
