44f95f30f6e4a659842d45f480754a5a0844f9e9
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-20 22:23:50 +0530

    Time: 4 ms (45.27%), Space: 5.7 MB (96.76%) - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,44 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+class Solution {
+public:
+    int reverse(int x) {
+         bool flag = false;
+        if(x < 0)flag = true;
+        x = abs(x);
+         long long ans = 0;
+        while(x){
+            ans = ans*10 + x%10;
+            x = x/10;
+        }
+        if(flag){
+           ans = -ans; 
+        }
+        if(ans < -pow(2,31) || ans > pow(2,31) - 1 )return 0;
+        return ans;
+    }
+};
