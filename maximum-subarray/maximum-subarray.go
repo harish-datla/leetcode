e1e994590245bd75609fb32747f9b1ed91e2e4f2
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-20 22:32:38 +0530

    Time: 4 ms (97.27%), Space: 3.3 MB (100.00%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.go
@@ -0,0 +1,46 @@
+import "math"
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+
+func max(i,j int) int{
+    if i < j {
+        return j
+    }
+    return i
+}
+func maxSubArray(nums []int) int {
+    csum := 0
+    cmax := math.MinInt32
+    for i := 0 ; i < len(nums) ; i++ {
+        csum += nums[i]
+        cmax = max(csum,cmax)
+        if csum < 0 {
+            csum = 0
+        }
+    }
+    return cmax;
+}
