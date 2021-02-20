63d588bdcb6673c92610492c507a739ef37c0d3c
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-20 22:04:55 +0530

    Time: 4 ms (96.39%), Space: 13.1 MB (91.85%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,41 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cmax = INT_MIN;
+        int csum = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            csum += nums[i];
+             cmax = max(csum,cmax);
+            if(csum < 0){
+                csum = 0;
+            }
+        }
+       return cmax; 
+    }
+};
