57b6a0a10426dcc1eba2b2716525f108909e2087
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-21 17:28:59 +0530

    Time: 20 ms (81.95%), Space: 16.9 MB (70.06%) - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,36 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            ans^=nums[i];
+        }
+        return ans;
+    }
+};
