651377b282a64a1c2efb63f7f7e520ae5a31458c
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-22 23:11:17 +0530

    Time: 4 ms (42.37%), Space: 2.2 MB (62.71%) - LeetHub

---
+++ ugly-number/ugly-number.go
@@ -0,0 +1,41 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+func isUgly(num int) bool {
+    if(num == 0){
+        return false;
+    }
+    for ;num%2 == 0; {
+        num/=2;
+    }
+    for ;num%3 == 0; {
+        num/=3;
+    }
+    for ;num%5 == 0; {
+        num/=5;
+    }
+    return num == 1;
+}
