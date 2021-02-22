112b3283fba8c249b684707a96ee847d5f670f13
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-22 23:06:37 +0530

    Time: 4 ms (38.98%), Space: 5.7 MB (90.19%) - LeetHub

---
+++ ugly-number/ugly-number.cpp
@@ -0,0 +1,38 @@
+/*
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
+// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
+// Democratic scientechnocracy.
+// Free and quality education for all.
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num == 0)return false;
+        while(num%2 == 0)num/=2;
+        while(num%3 == 0)num/=3;
+        while(num%5 == 0)num/=5;
+        
+        
+        return num == 1;
+    }
+};
