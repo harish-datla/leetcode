8966eb8a3c22808e49ad6e89796d68552e29aac0
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-02-24 16:25:42 +0530

    Update binary-subarrays-with-sum.go

--- binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,29 +1,4 @@
-/*
-                       _oo0oo_
-                      o8888888o
-                      88" . "88
-                      (| -_- |)
-                      0\  =  /0
-                    ___/`---'\___
-                  .' \\|     |// '.
-                 / \\|||  :  |||// \
-                / _||||| -:- |||||- \
-               |   | \\\  -  /// |   |
-               | \_|  ''\---/''  |_/ |
-               \  .-\__  '-'  ___/-. /
-             ___'. .'  /--.--\  `. .'___
-          ."" '<  `.___\_<|>_/___.' >' "".
-         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-         \  \ `_.   \_ __\ /__ _/   .-` /  /
-     =====`-.____`.___ \_____/___.-`___.-'=====
-                       `=---='
- 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
- SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
-// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
-// Democratic scientechnocracy.
-// Free and quality education for all.
+
 func numSubarraysWithSum(A []int, S int) int {
     ans , start,sum := 0,0,0
     for i := 0 ; i < len(A) ; i++{
