1ff5289c77a727e2c648beaa558a704b4b37dcee
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-03-04 02:24:19 +0530

    Time: 448 ms (78.28%), Space: 60.1 MB (39.00%) - LeetHub

--- count-good-meals/count-good-meals.cpp
+++ count-good-meals/count-good-meals.cpp
@@ -1,14 +1,21 @@
+static const auto _____ = []()
+{
+    // fast IO code : this I understand
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     long mod = 1e9+7;
     int countPairs(vector<int>& d) {
         unordered_map<long,long> mp;
         long long ans = 0;
-        
             for(int i = 0 ; i < d.size() ; i++){
                 for(int j = 1 ; j <= (1 << 22) ; j*=2){
                     if(mp.count(j - d[i])){
-                        ans = (ans%mod + mp[j - d[i]]%mod)%mod;
+                        ans = (ans + mp[j - d[i]])%mod;
                     }
                 }
                 mp[d[i]]+=1;
