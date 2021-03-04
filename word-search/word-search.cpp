5b94df3dbcd5e2b48d943d6a7840930426387308
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-03-04 20:34:36 +0530

    Time: 184 ms (43.70%), Space: 11 MB (89.75%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,27 +1,39 @@
-
 class Solution {
 public:
-    int m,n,k;
+    int m,n;
     bool check(vector<vector<char>>& board, const char* word, int i , int j){
-            //cout << m << " " << n << endl;
-        if(i < 0 || j < 0 || i >= m || j >= n || *word != board[i][j] || board[i][j] == '\0')return false;      
-        if(*(word+1) == '\0')return true;
-        char temp = board[i][j];
-        board[i][j] = '\0';
-        if(check(board,word+1,i-1,j) || check(board,word+1,i+1,j) || check(board,word+1,i,j-1)|| check(board,word+1,i,j+1))
+        if( i >= 0 && j >= 0 && i < m && j < n && *word == board[i][j] && board[i][j] != '\0'){ 
+            //cout << i << " " << j << endl;
+            if(*(word+1) == '\0')return true;
+            char temp = board[i][j];
+            board[i][j] = '\0';
+            if(check(board,word+1,i-1,j) || check(board,word+1,i+1,j) || check(board,word+1,i,j-1)|| check(board,word+1,i,j+1) ){
                         return true;
+            }
                
-        board[i][j] = temp;
+            board[i][j] = temp;
+            return false;
+        }
+        
+//         if(i >= 0 && j >= 0 && i < m && j < n && *word == board[i][j] && board[i][j] != '\0'){      
+//         if(*(word+1) == '\0')return true;
+//         char temp = board[i][j];
+//         board[i][j] = '\0';
+//         if(check(board,word+1,i-1,j) || check(board,word+1,i+1,j) || check(board,word+1,i,j-1)|| check(board,word+1,i,j+1))
+//                         return true;
+               
+//         board[i][j] = temp;
+//         return false;
+//         }
+        
+        
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> vis(board.size(),vector<bool>(board[0].size(),false));
-         m = board.size();
-         n = board[0].size();
-        k = word.size() ;
-        for(int i = 0 ; i < m ; i++)
-            for(int j = 0 ; j < n ; j++)
-            {
+        m = board.size();
+        n = board[0].size();
+        for(int i = 0 ; i < board.size() ; i++)
+            for(int j = 0 ; j < board[0].size() ; j++){
                     if(check(board,word.c_str(),i,j))
                         return true;
             }
@@ -29,4 +41,33 @@ public:
     }
 };
 
-
+// class Solution {
+//     public:
+//     	 bool exist(vector<vector<char> > &board, string word) {
+//     		 m=board.size();
+//     		 n=board[0].size();
+//             for(int x=0;x<m;x++)
+//                 for(int y=0;y<n;y++)
+//                 {
+//     				if(isFound(board,word.c_str(),x,y))
+//     					return true;
+//                 }
+//             return false;
+//         }
+//     private:
+//     	int m;
+//     	int n;
+//         bool isFound(vector<vector<char> > &board, const char* w, int x, int y)
+//         {
+//     		if(x<0||y<0||x>=m||y>=n||board[x][y]=='\0'||*w!=board[x][y])
+//     			return false;
+//             if(*(w+1)=='\0')
+//                 return true;
+//     		char t=board[x][y];
+//     		board[x][y]='\0';
+//     		if(isFound(board,w+1,x-1,y)||isFound(board,w+1,x+1,y)||isFound(board,w+1,x,y-1)||isFound(board,w+1,x,y+1))
+//     			return true; 
+//     		board[x][y]=t;
+//             return false;
+//         }
+//     };
