b4d8f3357f8a487d8092c776dafab2dc3a62c5f0
harish-datla <36021808+harish-datla@users.noreply.github.com>
2021-03-05 12:46:00 +0530

    Time: 4 ms (94.12%), Space: 8.9 MB (74.39%) - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -1,29 +1,4 @@
-/*
-                       _oo0oo_
-                      o8888888o
-                      88" . "88
-                      (| -_- |)
-                      0\  =  /0
-                    ___/`---'\___
-                  .' \\|     |// '.
-                 / \\|||  :  |||// \
-                / _||||| -:- |||||- \
-               |   | \\\  -  /// |   |
-               | \_|  ''\---/''  |_/ |
-               \  .-\__  '-'  ___/-. /
-             ___'. .'  /--.--\  `. .'___
-          ."" '<  `.___\_<|>_/___.' >' "".
-         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-         \  \ `_.   \_ __\ /__ _/   .-` /  /
-     =====`-.____`.___ \_____/___.-`___.-'=====
-                       `=---='
- 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
- SHAMELESLLY COPIED TEMPLATE FROM MULTIPLE SOURCES - baba_yaga
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/  
-// Acceptance and tolerance for all religions, including absence of beleif, pagans, spiritualism.
-// Democratic scientechnocracy.
-// Free and quality education for all.
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
