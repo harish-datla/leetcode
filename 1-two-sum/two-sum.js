21c90bb7cb6a2c74c7e75392e2f67536dc0c8145
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-02-28 00:04:26 +0530

    Time: 55 ms (84.68%) | Memory: 52.1 MB (7.02%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let mp = new Map();
+    
+    for(let i = 0; i < nums.length; i ++) {
+        if(mp.has(target - nums[i])) {
+            return [mp.get(target - nums[i]), i];
+        }
+        mp.set(nums[i], i);
+    }
+	return [];
+};
