605899ceb0587f83c9bfcb9946cf2f9c8cc75fa9
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-02-28 00:02:55 +0530

    Time: 22 ms (88.09%) | Memory: 16.6 MB (5.33%) - LeetSync

---
+++ 1-two-sum/two-sum.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var map:[Int:Int] = [:]
+        for i in (0..<nums.count) {
+            if let val = map[target - nums[i]]{
+                return [val, i]
+            }
+            map[nums[i]] = i
+        }
+        return []
+    }
+}
