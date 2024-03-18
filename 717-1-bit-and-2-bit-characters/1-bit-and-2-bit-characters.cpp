81582bec8ecf5dd163b241aacd6dce06ff28388d
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-18 21:20:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (28.29%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    bool isOneBitCharacter(vector<int>& bits, int idx = 0) {
+    bool isOneBitCharacter(vector<int>& bits) {
+        return isOneBit(bits, 0);   
+    }
+
+    bool isOneBit(vector<int>& bits, int idx ){
         if(idx == (bits.size() - 1) && bits[idx] == 0)return true;
         if(idx == bits.size()) return false;
         if(bits[idx] == 0)
-            return isOneBitCharacter(bits, idx+1);
+            return isOneBit(bits, idx+1);
         if(bits[idx] == 1)
-            return isOneBitCharacter(bits, idx+2);
-        return true;    
+            return isOneBit(bits, idx+2);
+        return true; 
     }
+
 };
