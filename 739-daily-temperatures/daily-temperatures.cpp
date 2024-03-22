3f10ae04a9b2d5ec84895bdb43e400bfd0a8527c
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-22 13:39:22 +0530

    Time: 99 ms (93.46%) | Memory: 109.8 MB (16.95%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
+        int n = t.size();
         stack<pair<int,int>> s;
-        vector<int> ans(t.size(),0);
-        for(int i = 0 ; i < t.size() ; i++){
+        vector<int> ans(n,0);
+        for(int i = 0 ; i < n ; i++){
             while(!s.empty() && s.top().first < t[i]){
                 ans[s.top().second] = i-s.top().second;
                 s.pop();
@@ -13,3 +14,11 @@ public:
         return ans;
     }
 };
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
