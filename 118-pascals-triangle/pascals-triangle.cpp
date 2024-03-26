02097512c0856f7f1c8f470993f03b91c6d30505
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-26 15:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (8.85%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     vector<vector<int>> pascalsTriangle;
 
-    Solution() {
-        pascalsTriangle.push_back({1});
-        for(int i = 1 ; i < 30 ; i++){
-            pascalsTriangle.push_back({1});
-            for(int j = 1 ; j < i  ; j++){
-                cout << i << " " << j << endl;
-                pascalsTriangle[i].push_back(pascalsTriangle[i-1][j-1] + pascalsTriangle[i-1][j]);
+    Solution() : pascalsTriangle(30) {
+        
+        for(int i = 0 ; i < 30 ; i++){
+            pascalsTriangle[i].assign(i+1,1);
+            for(int j = 1 ; j < i ; j++){
+                pascalsTriangle[i][j] = pascalsTriangle[i-1][j-1] + pascalsTriangle[i-1][j];
             }
-            pascalsTriangle[i].push_back({1});
         }
     }
     
