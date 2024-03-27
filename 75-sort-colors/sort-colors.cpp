b8516f64c3797c8067b155164464fb81273d5910
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-27 09:46:02 +0530

    Time: 27 ms (17.91%) | Memory: 9.7 MB (95.60%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,10 +3,15 @@ public:
     void sortColors(vector<int>& nums) {
         int start = 0, cur = 0, end = nums.size() - 1;
         while(cur <= end){
+            cout << start << " " << cur <<  " " << end <<  endl;
+            for(int i = 0 ; i < nums.size() ; i++){
+                cout << nums[i] << " ";
+            }
+            cout << endl;
             if(nums[cur] == 0){
                 swap(nums[start], nums[cur]);
+                if(cur == start)cur++;
                 start++;
-                cur++;
             }else if(nums[cur] == 1){
                 cur++;
             }else if(nums[cur] == 2){
