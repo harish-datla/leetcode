4f17bf203031b5c053d7a17c8371bedc4ab2140c
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-28 10:23:03 +0530

    Time: 4 ms (59.16%) | Memory: 23.3 MB (60.51%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
-        int ans = 0;
-        for(int i = 0 ; i < hours.size() ; i++){
-            if(hours[i] >= target)ans++;
-        }
-        return ans;
+        return count_if(hours.begin(),hours.end(),[&](int h){ return h >= target;});
     }
 };
