422af6d73e40575768d28dde9eca44d1097fd41f
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-04-01 14:42:57 +0530

    Time: 68 ms (91.26%) | Memory: 70.3 MB (10.06%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,8 +5,10 @@ public:
         int ans = INT_MIN;
         for(int i = 0 ; i < nums.size() ; i++){
             csum+=nums[i];
-            ans = max(csum,ans);
-            csum < 0?csum = 0: NULL ;
+            ans = max(csum, ans);
+            if(csum < 0){
+                csum = 0;
+            }
         }
         return ans;
     }
