ebb54f408504084ed64f4266e3f2c231dd3ebfc7
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-20 15:45:56 +0530

    Time: 13 ms (20.00%) | Memory: 26.9 MB (60.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -3,7 +3,7 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         if(nums.size() <= 1)return true;
         for(int i = 1 ; i < nums.size() ; i++){
-            if((nums[i]&1) == (nums[i-1]&1))return false;
+            if((nums[i]&1) ^ (nums[i-1]&1) == 0)return false;
         }
         return true;
     }
