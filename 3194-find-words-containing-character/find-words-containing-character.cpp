44af51caf491713e616afb812a93ece4ca24d42b
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-21 10:53:08 +0530

    Time: 10 ms (95.30%) | Memory: 33.2 MB (23.15%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    bool isCharInWord(char c, string word){
-        for(int i = 0 ; i < word.size() ; i++){
-            if(word[i] == c)return true;
-        }
-        return false;
-    }
-
     vector<int> findWordsContaining(vector<string>& words, char x) {
         ios_base::sync_with_stdio(false); 
         cin.tie(NULL); 
         vector<int> ans;
         for(int i = 0 ; i < words.size() ; i++){
-            if(isCharInWord(x,words[i]))
+            if(words[i].find(x) != string::npos)
                 ans.push_back(i);
         }
         return ans;
