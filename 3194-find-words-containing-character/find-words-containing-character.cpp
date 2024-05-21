6c8491fad8a15c9eee3e6ab4eb122bcfd6530ca1
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-21 10:52:00 +0530

    Time: 14 ms (82.24%) | Memory: 33.9 MB (13.30%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -8,6 +8,8 @@ public:
     }
 
     vector<int> findWordsContaining(vector<string>& words, char x) {
+        ios_base::sync_with_stdio(false); 
+        cin.tie(NULL); 
         vector<int> ans;
         for(int i = 0 ; i < words.size() ; i++){
             if(isCharInWord(x,words[i]))
