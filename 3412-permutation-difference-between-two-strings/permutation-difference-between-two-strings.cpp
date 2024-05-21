797e2d67e555383253fb365d7b9f50396722063d
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-21 10:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (80.88%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int findPermutationDifference(string s, string t) {
-       unordered_map<char,int> mp1;
-       unordered_map<char,int> mp2;
-       for(int i = 0 ; i < s.size() ; i++){
-         mp1[s[i]] = i;
-       }
-       for(int i = 0 ; i < t.size() ; i++){
-         mp2[t[i]] = i;
-       }
+       int pos[26] = {0};
        int ans = 0;
+       for(int i = 0 ; i < t.size() ; i++){
+         pos[s[i] - 'a'] = i;
+       }
        for(int i = 0 ; i < s.size() ; i++){
-            ans+=abs(mp1[s[i]] - mp2[s[i]]);
+            ans+=abs(i - pos[t[i] - 'a']);
        }
        return ans;
 
