23b8369366b45f91c26ce5a415175c0b6b341d5f
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-23 23:58:06 +0530

    Time: 50 ms (96.92%) | Memory: 78.3 MB (93.08%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
+        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
         vector<int> arr(pref.size());
         arr[0] = pref[0];
         for(int i = 1 ; i < pref.size() ; i++){
