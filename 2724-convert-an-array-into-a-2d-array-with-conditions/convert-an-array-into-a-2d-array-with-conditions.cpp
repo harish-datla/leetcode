5d5e6a81cf0dc42526b7f4890b72ad2b3bb60264
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-24 00:26:58 +0530

    Time: 20 ms (7.33%) | Memory: 33 MB (91.88%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        map<int,int> mp;
-        int maxFreq = INT_MIN;
-        for(int i = 0 ; i < nums.size() ; i++){
-            mp[nums[i]]+=1;
-            maxFreq = max(maxFreq,mp[nums[i]]);
-        }
-        //cout << maxFreq << endl;
-        vector<vector<int>> ans(maxFreq,vector<int>(1,-1));
-        //sort(nums.begin(),nums.end());
-        for(int i = 0 ; i < nums.size() ; i++){
-            //cout << i << endl;
-          if(ans[mp[nums[i]] - 1][0] == -1){
-            ans[mp[nums[i]] - 1][0] = nums[i];
-          }
-          else{
-            ans[mp[nums[i]] - 1].push_back(nums[i]);
-          }
-          mp[nums[i]]--;  
+        vector<int> count(nums.size() + 1);
+        vector<vector<int>> ans;
+        for(int x : nums){
+            if(ans.size() <= count[x]){
+                ans.push_back({});
+            }
+            ans[count[x]++].push_back(x);
         }
         return ans;
     }
