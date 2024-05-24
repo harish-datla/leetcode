faca96a3d5cddf291113c30f9186130f0a791492
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-24 12:05:42 +0530

    Time: 96 ms (26.43%) | Memory: 62.9 MB (89.56%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -12,13 +12,9 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        if(!root)return 0;
-        if(root->val < low){
-            return rangeSumBST(root->right, low, high);
-        }
-        if(root->val > high){
-            return rangeSumBST(root->left, low, high);
-        }
-        return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high) + root->val;
+       if(!root)return 0;
+       if(root->val < low)return rangeSumBST(root->right, low, high);
+       if(root->val > high)return rangeSumBST(root->left, low, high);
+       return rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high) + root->val;
     }
 };
