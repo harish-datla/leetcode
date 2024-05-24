a32ac1b99719b05f04f234debdd9181ca5646648
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-05-24 19:26:41 +0530

    Time: 39 ms (5.69%) | Memory: 76.2 MB (20.34%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -8,44 +8,38 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-const static auto ioSpeedUp = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    return NULL;
-}();
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        ListNode* ptr = new ListNode();
-        ListNode* ans = ptr;
-
+        ListNode* head = new ListNode();
+        ListNode* ptr = head;
         while(l1 && l2){
-            ptr->next = new ListNode((l1->val + l2->val + carry)%10);
-            carry = (l1->val + l2->val + carry)/10;
+            int val = (l1->val + l2->val+carry)%10;
+            carry = (l1->val + l2->val+carry)/10;
+            ptr->next =  new ListNode(val);
             l1 = l1->next;
             l2 = l2->next;
             ptr = ptr->next;
         }
         while(l1){
-            ptr->next = new ListNode((l1->val + carry)%10);
+            int val = (l1->val + carry)%10;
             carry = (l1->val + carry)/10;
+            ptr->next =  new ListNode(val);
             l1 = l1->next;
-            ptr=ptr->next;
+            ptr = ptr->next;
         }
         while(l2){
-
-            ptr->next = new ListNode((l2->val + carry)%10);
+            int val = (l2->val + carry)%10;
             carry = (l2->val + carry)/10;
+            ptr->next =  new ListNode(val);
             l2 = l2->next;
-            ptr=ptr->next;
+            ptr = ptr->next;
+        }
+        if(carry){
+            ptr->next =  new ListNode(carry);
         }
 
-        if(carry){
-            ptr->next = new ListNode((carry)%10);
-        }
-        return ans->next;
+        return head->next;
     }
 };
