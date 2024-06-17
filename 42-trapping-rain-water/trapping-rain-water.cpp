b3692ea9aff0d37e5e0ad35e680230f80685ba04
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-06-17 22:12:15 +0530

    Time: 12 ms (40.74%) | Memory: 23.1 MB (28.97%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if(height.size() <= 2)return 0;
+       stack<int> s;
+       int idx = 0;
+       int water = 0;
+       while(idx < height.size()){
+          if(s.empty() || height[s.top()] >= height[idx]){
+            s.push(idx);
+            idx++;
+          }else{
+            int pre = s.top();
+            s.pop();
+            if(!s.empty()){
+                int minboundaryHeight = min(height[s.top()], height[idx]);
+                water+=(minboundaryHeight - height[pre])*(idx - s.top() - 1);
+            }
+          }  
+       } 
+       return water;
+    }
+};
