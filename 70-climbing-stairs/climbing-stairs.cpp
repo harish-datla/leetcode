f9b67afe7ad09e73389e0bd9f9a1a853f1322a0e
harish-datla <36021808+harish-datla@users.noreply.github.com>
2025-08-29 21:41:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.10%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        vector<int> a(n+1,0);
+        a[0] = 0;
+        a[1] = 1;
+        a[2] = 2;
+        for(int i = 3 ; i < n+1 ; i++){
+          a[i] = a[i-1]+a[i-2];
+        }
+        return a[n];
+    }
+};
