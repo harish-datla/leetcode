2d72db46db0f2dd94e141977772c4a4b725c6746
harish-datla <36021808+harish-datla@users.noreply.github.com>
2025-09-07 00:09:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (77.68%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -5,32 +5,26 @@ public:
         if (n == 1) return nums[0];
         if (n == 2) return max(nums[0], nums[1]);
 
-        int maxRobbedOverall = -1;
+        int prev, curr, temp;
 
         // Case 1: Rob houses [0 .. n-2]
-        {
-            int prev = nums[0];
-            int curr = max(nums[0], nums[1]);
-            for (int i = 2; i < n - 1; i++) {
-                int temp = curr;
-                curr = max(nums[i] + prev, curr);
-                prev = temp;
-            }
-            maxRobbedOverall = curr;
+        prev = nums[0];
+        curr = max(nums[0], nums[1]);
+        for (int i = 2; i < n - 1; i++) {
+            temp = curr;
+            curr = max(nums[i] + prev, curr);
+            prev = temp;
         }
+        int result = curr;
 
         // Case 2: Rob houses [1 .. n-1]
-        {
-            int prev = nums[1];
-            int curr = max(nums[1], nums[2]);
-            for (int i = 3; i < n; i++) {
-                int temp = curr;
-                curr = max(nums[i] + prev, curr);
-                prev = temp;
-            }
-            maxRobbedOverall = max(maxRobbedOverall, curr);
+        prev = nums[1];
+        curr = max(nums[1], nums[2]);
+        for (int i = 3; i < n; i++) {
+            temp = curr;
+            curr = max(nums[i] + prev, curr);
+            prev = temp;
         }
-
-        return maxRobbedOverall;
+        return max(result, curr);
     }
 };
