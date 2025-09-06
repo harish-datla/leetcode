1069182d2d883d8850e735aa4834bb8d8bfad5fe
harish-datla <36021808+harish-datla@users.noreply.github.com>
2025-09-07 00:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.64%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-
-    int linearHouseRobber(vector<int>& nums, int l , int r){
-        if(r == l)return nums[l];
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1)return nums[0];
+        if(nums.size() == 2)return max(nums[0], nums[1]);
+        int maxRobbedOverall = -1;
+        int l = 0;
+        int r = nums.size() - 2;
         int maxRobbedTillPrevHouse = nums[l];
         int maxRobbedTillCurrHouse = max(nums[l],nums[l+1]);
         for(int i = l+2 ; i <= r ; i++){
@@ -10,10 +13,20 @@ public:
             maxRobbedTillCurrHouse = max(nums[i] + maxRobbedTillPrevHouse , maxRobbedTillCurrHouse);
             maxRobbedTillPrevHouse = newMaxRobbedTillPrevHouse;
         } 
-        return maxRobbedTillCurrHouse;
-    }
-    int rob(vector<int>& nums) {
-        if(nums.size() == 1)return nums[0];
-        return max(linearHouseRobber(nums, 0, nums.size() - 2), linearHouseRobber(nums, 1, nums.size() - 1));
+        maxRobbedOverall = maxRobbedTillCurrHouse;
+        
+        l = 1;
+        r = nums.size() - 1;
+        maxRobbedTillPrevHouse = nums[l];
+        maxRobbedTillCurrHouse = max(nums[l],nums[l+1]);
+        for(int i = l+2 ; i <= r ; i++){
+            int newMaxRobbedTillPrevHouse = maxRobbedTillCurrHouse;
+            maxRobbedTillCurrHouse = max(nums[i] + maxRobbedTillPrevHouse , maxRobbedTillCurrHouse);
+            maxRobbedTillPrevHouse = newMaxRobbedTillPrevHouse;
+        } 
+        maxRobbedOverall = max(maxRobbedOverall,maxRobbedTillCurrHouse);
+        
+
+        return maxRobbedOverall;
     }
 };
